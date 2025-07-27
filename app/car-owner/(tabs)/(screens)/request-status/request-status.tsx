87e3534e35feba2970cc7035eff959723c1391e3
GlensonAnsin <ansin.glenson01@gmail.com>
2025-07-28 01:52:08 +0800

    feat: display request status with status colors for pending, rejected, ongoing, and completed

--- app/car-owner/(tabs)/(screens)/request-status/request-status.tsx
+++ app/car-owner/(tabs)/(screens)/request-status/request-status.tsx
@@ -10,6 +10,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const RequestStatus = () => {
     dayjs.extend(utc);
     dayjs.extend(timezone);
+    const guessTimezone = dayjs.tz.guess();
     
     const [requestStatus, setRequestStatus] = useState<{ vehicleName: string, repairShop: string, scanReference: string, datetime: string, status: string }[]>([]);
 
@@ -27,13 +28,13 @@ const RequestStatus = () => {
                         const scanReference = diagnostic.scan_reference;
                         diagnostic.mechanic_requests?.forEach((request: any) => {
                             const repairShop = res2.find((shop: any) => shop.repair_shop_id === request.repair_shop_id);
-                            const datetime = dayjs(request.request_datetime).utc(true).tz('Asia/Manila').format()
+                            const datetime = dayjs(request.request_datetime).utc(true).tz(guessTimezone).format()
                             if (repairShop) {
                                 statusData.push({
                                     vehicleName,
                                     repairShop: repairShop.shop_name,
                                     scanReference,
-                                    datetime: dayjs(datetime).utc(true).tz('Asia/Manila').format("ddd MMM DD YYYY, h:mm A"),
+                                    datetime: dayjs(datetime).utc(true).tz(guessTimezone).format("ddd MMM DD YYYY, h:mm A"),
                                     status: request.status,
                                 });
                             }
@@ -75,9 +76,31 @@ const RequestStatus = () => {
             <Header headerTitle='Request Status' link='/car-owner/(tabs)' />
 
             <View style={styles.lowerBox}>
-                <TouchableOpacity onPress={() => console.log(requestStatus)}>
-                    <Text>Button</Text>
-                </TouchableOpacity>
+                {grouped && (
+                    <>
+                        {grouped.map((item, index) => (
+                            <TouchableOpacity key={index} style={styles.requestButton}>
+                                <View style={styles.vehicleShopContainer}>
+                                    <Text style={styles.vehicleName}>{item.vehicleName}</Text>
+                                    <Text style={styles.requestText}>{item.repairShop}</Text>
+                                    <Text style={styles.requestText}>{item.datetime}</Text>
+                                </View>
+                                <View style={styles.statusContainer}>
+                                    <Text style={styles.statusText}>{item.status}</Text>
+                                    <View style={[styles.statusColor, { backgroundColor:
+                                        item.status === 'Pending'
+                                        ? '#FFC107'
+                                        : item.status === 'Rejected'
+                                        ? '#F44336'
+                                        : item.status === 'Ongoing'
+                                        ? '#2196F3'
+                                        : '#4CAF50'
+                                    }]}></View>
+                                </View>
+                            </TouchableOpacity>
+                        ))}
+                    </>
+                )}
             </View>
         </ScrollView>
         </SafeAreaView>
@@ -94,6 +117,52 @@ const styles = StyleSheet.create({
         marginTop: 20,
         marginBottom: 100,
         width: '90%',
+    },
+    requestButton: {
+        width: '100%',
+        backgroundColor: '#EAEAEA',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        marginBottom: 10,
+    },
+    vehicleShopContainer: {
+
+    },
+    vehicleName: {
+        fontFamily: 'LeagueSpartan_Bold',
+        color: '#333',
+        fontSize: 18,
+    },
+    requestText: {
+        fontFamily: 'LeagueSpartan_Bold',
+        color: '#555',
+        fontSize: 14,
+    },
+    statusContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+    },
+    statusColor: {
+        width: 10,
+        height: 10,
+        borderRadius: 10
+    },
+    statusText: {
+        fontFamily: 'LeagueSpartan_Bold',
+        color: '#555',
+        fontSize: 14,
     },
 })
 
